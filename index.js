df0eb927afe8d75ae342d506c88c2639e5eed3a5
mdshahjhan <mdshahjha478877@gmail.com>
2022-10-22 17:03:33 +0600

    server site

--- index.js
+++ index.js
@@ -6,16 +6,42 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./Data/categories.json');
+const news = require('./Data/news.json');
 
 app.get('/',(req,res)=>{
     res.send('News API Running');
 });
-app.get('/news-categories',(req,res)=>{
+app.get('/news-categories', (req, res) => {
     res.send(categories);
+});
+app.get('/category/:id',(req,res)=>{
+    const id  = req.params.id;
+    if(id==='08'){
+        res.send(news);
+
+    }
+    else{
+        const category_news = news.filter(n=>n.category_id === id);
+        res.send(category_news);
+       
+       
+    }
+   
+})
+app.get('/news',(req,res)=>{
+    res.send(news);
+})
+app.get('/news/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedNews = news.find(n=>n._id===id)
+    res.send(selectedNews);
+    console.log(req.params)
 })
 
 app.listen(port,()=>{
     console.log('Dragon News Server running  on port ',port)
 
 })
+
+
 
